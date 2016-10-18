06cadf52bc4cfb6ba7eb7e1aca75256f122f20a0
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-10-18 12:03:28 +0200

    use Doctrine DBAL to manage database access

--- app/app.php
+++ app/app.php
@@ -3,9 +3,26 @@
 	use Symfony\Component\Debug\ErrorHandler;
 	use Symfony\Component\Debug\ExceptionHandler;
 	
+	// register global error and exception handlers
 	ErrorHandler::register();
 	ExceptionHandler::register();
 	
+	// register service providers
+	$app->register(new Silex\Provider\DoctrineServiceProvider());
 	$app->register(new Silex\Provider\RoutingServiceProvider());
+	
+	//register services
+	$app['dao.group'] = new Compta\DAO\GroupDAO($app['db']);
+	$app['dao.user'] = function($app) {
+		$userDAO = new Compta\DAO\UserDAO($app['db']);
+		$userDAO->setGroupDAO($app['dao.group']);
+		return $userDAO;
+	};
+	$app['dao.depense'] = function($app) {
+		$depenseDAO = new Compta\DAO\DepenseDAO($app['db']);
+		$depenseDAO->setGroupDAO($app['dao.group']);
+		$depenseDAO->setUserDAO($app['dao.user']);
+		return $depenseDAO;
+	};
 	
 ?>
--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@
 	
 	$app = new Silex\Application();
 	
+	require_once __DIR__.'/../app/config/dev.php';
 	require_once __DIR__.'/../app/app.php';
 	require_once __DIR__.'/../app/routes.php';
 	
