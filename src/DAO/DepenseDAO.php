a20b5004bc95a0c2ddc6215ac37138985aeca4f6
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-10-19 16:50:12 +0200

    DepenseDAO - update get() + write save()

--- src/DAO/DepenseDAO.php
+++ src/DAO/DepenseDAO.php
@@ -6,15 +6,27 @@
 	
 	class DepenseDAO extends DAO {
 		
-		private function get($id) {
+		public function get($id) {
 			$query = $this->getDb()->createQueryBuilder();
 			$query->select('*')
-			      ->from('users')
+			      ->from('depenses')
 			      ->where('id = :id')
 			      ->setParameter(':id', $id);
 			$statement = $query->execute();
 			$statement->setFetchMode(\PDO::FETCH_CLASS, 'Compta\Domain\Depense');
-			return $statement->fetch();
+			$depense = $statement->fetch();
+			$query = $this->getDb()->createQueryBuilder();
+			$query->select('*')
+			      ->from('mapping_users')
+			      ->where('depense_id = :depense_id')
+			      ->setParameter(':depense_id', $depense->getId());
+			$answer = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+			$users = [];
+			foreach ($answer as $row) {
+				$users[] = $row['user_id'];
+			}
+			$depense->setUsers($users);
+			return $depense;
 		}
 		
 		public function findByGroup($group_id) {
@@ -40,15 +52,65 @@
 			return $depenses;
 		}
 		
+		public function save(Depense $depense) {
+			// create/update entry in 'depenses' table
+			$data = array(
+				'montant' => $depense->getMontant(),
+				'date' => $depense->getDate(),
+				'name' => $depense->getName(),
+				'group_id' => $depense->getGroupId(),
+				'user_id' => $depense->getUserId(),
+			);
+			$id = $depense->getId();
+			if ( $id != NULL)
+				$this->getDb()->update('depenses', $data, array('id' => $id));
+			else {
+				$this->getDb()->insert('depenses', $data);
+				$id = $this->getDb()->lastInsertId();
+				$depense->setId($id);
+			}
+			// get entries from 'mapping_users' table
+			$query = $this->getDb()->createQueryBuilder();
+			$query->select('*')
+			      ->from('mapping_users')
+			      ->where('depense_id = :depense_id')
+			      ->setParameter(':depense_id', $depense->getId());
+			$answer = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+			$users = $depense->getUsers();
+			// create entries in 'mapping_users' table
+			foreach ($users as $user) {
+				$found = false;
+				foreach ($answer as $row)
+					if ($row['user_id'] == $user) $found = true;
+				if (!$found)
+					$this->getDb()->insert('mapping_users', array(
+						'depense_id' => $id,
+						'user_id' => $user
+					));
+			}
+			// remove entries in 'mapping_users' table
+			foreach ($answer as $row) {
+				$found = false;
+				foreach ($users as $group)
+					if ($row['group_id'] == $user) $found = true;
+				if (!$found) {
+					$query = $this->getDb()->createQueryBuilder();
+					$query->delete('*')
+					      ->from('mapping_users')
+					      ->where('depense_id = :depense_id')
+					      ->andWhere('user_id = :user_id')
+					      ->setParameter(':depense_id', $id)
+					      ->setParameter(':user_id', $user)
+					      ->execute();
+				}
+			}
+		}
+		
 		public function deleteByGroup($group_id) {
 			//TODO
 		}
 		
 		public function deleteByUser($user_id) {
-			//TODO
-		}
-		
-		public function save(Depense $depense) {
 			//TODO
 		}
 		
