4b899b726ee91162e18d876b89959e63e300448d
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-10-20 14:30:53 +0200

    DepenseDAO - update save() + write deleteByUser()

--- src/DAO/DepenseDAO.php
+++ src/DAO/DepenseDAO.php
@@ -22,11 +22,7 @@
 			      ->where('depense_id = :depense_id')
 			      ->setParameter(':depense_id', $depense->getId());
 			$answer = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
-			$users = [];
-			foreach ($answer as $row) {
-				$users[] = $row['user_id'];
-			}
-			$depense->setUsers($users);
+			foreach ($answer as $row) $depense->addUser($row['user_id']);
 			return $depense;
 		}
 		
@@ -93,17 +89,12 @@
 			foreach ($answer as $row) {
 				$found = false;
 				foreach ($users as $group)
-					if ($row['group_id'] == $user) $found = true;
-				if (!$found) {
-					$query = $this->getDb()->createQueryBuilder();
-					$query->delete('*')
-					      ->from('mapping_users')
-					      ->where('depense_id = :depense_id')
-					      ->andWhere('user_id = :user_id')
-					      ->setParameter(':depense_id', $id)
-					      ->setParameter(':user_id', $user)
-					      ->execute();
-				}
+					if ($row['user_id'] == $user) $found = true;
+				if (!$found)
+					$this->getDb()->delete('mapping_users', array(
+						'depense_id' => $id,
+						'user_id' => $user
+					));
 			}
 		}
 		
@@ -111,11 +102,40 @@
 			$this->getDb()->delete('depenses', array('id' => $id));
 		}
 		
-		public function deleteByGroup($group_id) {
-			//TODO
+		public function deleteByUser($user_id) {
+			// remove dependent entries from 'depenses' table
+			$query = $this->getDb()->createQueryBuilder();
+			$query->select('*')
+			      ->from('depenses')
+			      ->where('user_id = :user_id')
+			      ->setParameter(':user_id', $user_id);
+			$statement = $query->execute();
+			$statement->setFetchMode(\PDO::FETCH_CLASS, 'Compta\Domain\Depense');
+			$depenses = $statement->fetchAll();
+			foreach ($depenses as $depense) {
+				$id = $depense->getId();
+				$this->delete($id);
+				$this->getDb()->delete('mapping_users', array('depense_id' => $id));
+			}
+			// get entries from 'mapping_users' table
+			$query = $this->getDb()->createQueryBuilder();
+			$query->select('*')
+			      ->from('mapping_users')
+			      ->where('user_id = :user_id')
+			      ->setParameter(':user_id', $user_id);
+			$answer = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+			// remove unreferenced entries from 'depenses' table
+			foreach ($answer as $row) {
+				$id = $row['depense_id'];
+				$depense = $this->get($id)->removeUser($user_id);
+				if ($depense->getUsers() == NULL) $this->delete($id);
+				else $this->save($depense);
+			}
+			// remove entries from 'mapping_users' table
+			$this->getDb()->delete('mapping_users', array('user_id' => $user_id));
 		}
 		
-		public function deleteByUser($user_id) {
+		public function deleteByGroup($group_id) {
 			//TODO
 		}
 		
