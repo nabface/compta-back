2db3b90fcd266d98fd2d1718af31cbd6273ffa0f
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-10-24 15:34:12 +0200

    APIControllerDelete - checks that an admin is logged in before allowing admin tasks

--- src/Controller/APIControllerDelete.php
+++ src/Controller/APIControllerDelete.php
@@ -3,58 +3,70 @@
 	namespace Compta\Controller;
 	
 	use Silex\Application;
+	use Symfony\Component\HttpFoundation\Request;
 	
 	class APIControllerDelete {
 		
-		public function deleteDepense($id, Application $app) {
-			// TODO - check that user is logged in as admin
-			if ($app['dao.depense']->get($id)) {
-				$app['dao.depense']->delete($id);
-				return $app->json(array(
-					'status' => 'OK'
-				), 200);
+		use Security;
+		
+		public function deleteDepense($id, Request $request, Application $app) {
+			$json = $this->isLoggedIn($request, $app);
+			if ($json === NULL) {
+				if ($app['dao.depense']->get($id)) {
+					$app['dao.depense']->delete($id);
+					$json = $app->json(array(
+						'status' => 'OK'
+					), 200);
+				}
+				else {
+					$json = $app->json(array(
+						'status' => 'KO',
+						'error' => 'Pas de dépense enregistrée avec l’id '.$id
+					), 400);
+				}
 			}
-			else {
-				return $app->json(array(
-					'status' => 'KO',
-					'error' => 'Pas de dépense enregistrée avec l’id '.$id
-				), 400);
-			}
+			return $json;
 		}
 		
-		public function deleteUser($id, Application $app) {
-			// TODO - check that user is logged in as admin
-			if ($app['dao.user']->get($id)) {
-				$app['dao.depense']->deleteByUser($id);
-				$app['dao.user']->delete($id);
-				return $app->json(array(
-					'status' => 'OK'
-				), 200);
+		public function deleteUser($id, Request $request, Application $app) {
+			$json = $this->isLoggedIn($request, $app);
+			if ($json === NULL) {
+				if ($app['dao.user']->get($id)) {
+					$app['dao.depense']->deleteByUser($id);
+					$app['dao.user']->delete($id);
+					$json = $app->json(array(
+						'status' => 'OK'
+					), 200);
+				}
+				else {
+					$json = $app->json(array(
+						'status' => 'KO',
+						'error' => 'Pas d’utilisateur enregistré avec l’id '.$id
+					), 400);
+				}
 			}
-			else {
-				return $app->json(array(
-					'status' => 'KO',
-					'error' => 'Pas d’utilisateur enregistré avec l’id '.$id
-				), 400);
-			}
+			return $json;
 		}
 		
-		public function deleteGroup($id, Application $app) {
-			// TODO - check that user is logged in as admin
-			if ($app['dao.group']->get($id)) {
-				$app['dao.depense']->deleteByGroup($id);
-				$app['dao.user']->removeFromGroup($id);
-				$app['dao.group']->delete($id);
-				return $app->json(array(
-					'status' => 'OK'
-				), 200);
+		public function deleteGroup($id, Request $request, Application $app) {
+			$json = $this->isLoggedIn($request, $app);
+			if ($json === NULL) {
+				if ($app['dao.group']->get($id)) {
+					$app['dao.depense']->deleteByGroup($id);
+					$app['dao.user']->removeFromGroup($id);
+					$app['dao.group']->delete($id);
+					$json = $app->json(array(
+						'status' => 'OK'
+					), 200);
+				}
+				else {
+					$json = $app->json(array(
+						'status' => 'KO',
+						'error' => 'Pas de groupe enregistré avec l’id '.$id
+					), 400);
+				}
 			}
-			else {
-				return $app->json(array(
-					'status' => 'KO',
-					'error' => 'Pas de groupe enregistré avec l’id '.$id
-				), 400);
-			}
+			return $json;
 		}
 		
 	}
