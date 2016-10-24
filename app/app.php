c0be0359bbcff30bf028a5a08258b5bbc0ccf3bd
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-10-24 14:19:26 +0200

    disable session management by Silex

--- app/app.php
+++ app/app.php
@@ -11,7 +11,6 @@
 	// register service providers
 	$app->register(new Silex\Provider\DoctrineServiceProvider());
 	$app->register(new Silex\Provider\RoutingServiceProvider());
-	$app->register(new Silex\Provider\SessionServiceProvider());
 	$app->register(new Silex\Provider\MonologServiceProvider(), array(
 		'monolog.logfile' => __DIR__.'/../logs/dev.log',
 		'monolog.name' => 'Compta',
